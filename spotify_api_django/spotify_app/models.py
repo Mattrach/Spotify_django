69594c1150b317c716465792eaabf99a98437db6
Adrien López <78029686+Mattrach@users.noreply.github.com>
2025-02-20 16:31:19 +0100

    Update models.py

--- spotify_api_django/spotify_app/models.py
+++ spotify_api_django/spotify_app/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 class MusicalPreference(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # Relación con el modelo de usuario estándar
-    genero_favorito = models.CharField(max_length=100, blank=True, null=True)  # Género favorito
-    artista_favorito = models.CharField(max_length=100, blank=True, null=True)  # Artista favorito
-    cancion_favorita = models.CharField(max_length=100, blank=True, null=True)  # Canción favorita
+    genero_favorito = models.CharField(max_length=100, blank=True, null=True)
+    artista_favorito = models.CharField(max_length=100, blank=True, null=True)
+    cancion_favorita = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"Preferencias musicales de {self.user.username}."
